99e6d398b414098401ff967e2bbb2b637242c9fc
medjik <med.akhmetov@gmail.com>
2023-07-11 11:12:16 +0600

    First attempt

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int firstDigit = number/1000;
+        int secondDigit = (number%1000)/100;
+        int thirdDigit = (number%100)/10;
+        int fourthDigit = number%10;
+        System.out.println(firstDigit+secondDigit+thirdDigit+fourthDigit);
     }
 }
