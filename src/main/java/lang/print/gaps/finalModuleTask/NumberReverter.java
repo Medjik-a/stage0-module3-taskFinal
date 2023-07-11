8e515319c7edda1e0936fbecc20d8ff732347124
medjik <med.akhmetov@gmail.com>
2023-07-11 11:21:17 +0600

    third attempt

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,6 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
-        System.out.println((number/100)+((number%10)*100)+((number%100)/10));
+        System.out.println((number/100)+((number%10)*100)+((number%100)/10)*10);
     }
 }
